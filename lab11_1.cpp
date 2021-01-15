93428af3b6f3e03537e871345f9517f2517d03c8
Baangkok Vanijyananda <iambaangkok@gmail.com>
2021-01-15 16:03:32 +0700

    Lab11_1 completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
+typedef long long int lli;
+
+lli fibonacci(lli);
 
 int main(){
-    int x;
+    lli x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+lli fibonacci(lli n){
+    if(n == 0){
+        return 0;
+    }
+    if(n == 1){
+        return 1;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
 }
